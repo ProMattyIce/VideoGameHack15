2da3aaca650c3edb53a4109455f2ad52f63642f9
Matthew Hoare <pro.mathaore013@gmail.com>
2015-04-11 09:49:41 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String args[])
+    {
+        MetaMindResults sample = new MetaMindResults("terriable");
+       System.out.println(sample.toString());
+
+        Game halo = new Game("Heros of the Storm");
+
+    }
+}
